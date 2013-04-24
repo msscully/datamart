a2aab65e672841541a0cb16c7867c8dfa5bc118b
Mark Scully <mark.s.scully@gmail.com>
2013-04-24 12:48:38 -0500

    Added SQLALCHEMY_DATABASE_URI to config files.

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ BOOTSTRAP_USE_MINIFIED = True
 BOOTSTRAP_USE_CDN = True
 BOOTSTRAP_FONTAWESOME = True
 SECRET_KEY = "\xdb\xf1\xf6\x14\x88\xd4i\xda\xbc/E'4\x7f`iz\x98r\xb9s\x1c\xca\xcd"
+SQLALCHEMY_DATABASE_URI = ''

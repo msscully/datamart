a6ea5d85847fb54dec3ab498af87e654a0799a21
Mark Scully <mark.s.scully@gmail.com>
2013-04-24 12:48:04 -0500

    Added debug toolbar.

--- datamart/__init__.py
+++ datamart/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.bootstrap import Bootstrap
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config.from_object('config')
 app.config.from_envvar('DATAMART_APP_SETTINGS')
 Bootstrap(app)
+
+toolbar = DebugToolbarExtension(app)
 
 db = SQLAlchemy(app)
 

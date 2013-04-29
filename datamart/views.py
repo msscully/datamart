f0e8cd7e1da6bda1b7baceb15ada45639508cbf9
Mark Scully <mark.s.scully@gmail.com>
2013-04-29 10:13:09 -0500

    Fixed method syntax.

--- datamart/views.py
+++ datamart/views.py
@@ -2,17 +2,17 @@ from flask import render_template
 from datamart import app
 from forms import ExampleForm
 
-@app.route('/', methods=('GET', 'POST',))
+@app.route('/', methods=['GET', 'POST',])
 def index():
     form = ExampleForm()
     if form.validate_on_submit():
         return "PASSED"
     return render_template('index.html', form=form)
 
-@app.route('/dimensions', methods=('GET'))
+@app.route('/dimensions', methods=['GET'])
 def dimensions():
     return render_template('dimensions.html')
 
-@app.route('/variables', methods=('GET'))
+@app.route('/variables', methods=['GET'])
 def variables():
     return render_template('variables.html')

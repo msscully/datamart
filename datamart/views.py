3241f6e79b51c76be90354c8cb38bb30ec320031
Mark Scully <mark.s.scully@gmail.com>
2013-04-29 10:10:15 -0500

    Moved common content to base template and added empty variables and dimensions templates.

--- datamart/views.py
+++ datamart/views.py
@@ -7,5 +7,12 @@ def index():
     form = ExampleForm()
     if form.validate_on_submit():
         return "PASSED"
-    return render_template('example.html', form=form)
+    return render_template('index.html', form=form)
 
+@app.route('/dimensions', methods=('GET'))
+def dimensions():
+    return render_template('dimensions.html')
+
+@app.route('/variables', methods=('GET'))
+def variables():
+    return render_template('variables.html')

87df8becf5602a2713b88ad17be78ce015206cf3
Mark Scully <mark.s.scully@gmail.com>
2013-05-02 10:44:14 -0500

    Finished flask-bootstrap removal, added js block to base, simple admin version.

--- datamart/__init__.py
+++ datamart/__init__.py
@@ -1,12 +1,10 @@
 from flask import Flask, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.bootstrap import Bootstrap
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config.from_object('config')
 app.config.from_envvar('DATAMART_APP_SETTINGS')
-Bootstrap(app)
 
 toolbar = DebugToolbarExtension(app)
 
@@ -15,3 +13,4 @@ db = SQLAlchemy(app)
 import datamart.models
 import datamart.views
 import datamart.api
+#import admin
---
+++ datamart/admin.py
@@ -0,0 +1,8 @@
+from flask.ext.admin import Admin, BaseView, expose
+from flask.ext.admin.contrib.sqlamodel import ModelView
+from datamart import app, db, models
+
+#TODO Make name configurable
+admin = Admin(app, name='Datamart Admin', url='/admin')
+admin.add_view(ModelView(models.Dimension, db.session))
+admin.add_view(ModelView(models.Variable, db.session))

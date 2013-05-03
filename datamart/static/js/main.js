a24ae88a111733419c98817854a68889ea60393c
Mark Scully <mark.s.scully@gmail.com>
2013-05-03 13:18:56 -0500

    Now using backbone and backgrid with require.js.

---
+++ datamart/static/js/main.js
@@ -0,0 +1,5 @@
+require(['ext/jquery', 'ext/underscore', 'ext/backbone', 'ext/backgrid', 'ext/json2', 'dimensions', 'datasource'], function() {
+    $(document).ready(function() {
+        //window.App = new TodoApp({ appendTo: $('body') });
+    });
+});

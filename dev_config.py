e231e48b259314609c4b2946356bb1db9d47d495
Mark Scully <mark.s.scully@gmail.com>
2013-05-09 15:47:17 -0500

    Added initial facts model, api, and admin.  Admin currently not displaying hstore.

--- dev_config.py
+++ dev_config.py
@@ -3,4 +3,4 @@ BOOTSTRAP_USE_MINIFIED = False
 BOOTSTRAP_USE_CDN = False
 BOOTSTRAP_FONTAWESOME = True
 SECRET_KEY = "\xdb\xf1\xf6\x14\x88\xd4i\xda\xbc/E'4\x7f`iz\x98r\xb9s\x1c\xca\xcd"
-SQLALCHEMY_DATABASE_URI = 'sqlite:///datamart.db'
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://datamart:datamart@localhost/datamart'

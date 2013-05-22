fb91dd06b969b676f291d7ac0ca5fd97955f9143
Mark Scully <mark.s.scully@gmail.com>
2013-05-22 09:47:43 -0500

    Removed unused js files.

--- datamart/static/js/app.js
+++
@@ -1,6 +0,0 @@
-var DatamartApp = angular.module("DatamartApp", ["ngResource"]).
-    config(function($routeProvider) {
-        $routeProvider.
-            when('/', { controller: ListCtrl, templateUrl: 'list.html' }).
-            otherwise({ redirectTo: '/' });
-    });
--- datamart/static/js/main.js
+++
@@ -1,5 +0,0 @@
-require(['ext/jquery', 'ext/underscore', 'ext/backbone', 'ext/backgrid', 'ext/json2', 'dimensions'], function() {
-    $(document).ready(function() {
-        //window.App = new TodoApp({ appendTo: $('body') });
-    });
-});

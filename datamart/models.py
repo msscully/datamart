15f639d6758763469c673ab69f3b5026c2eed379
Mark Scully <mark.s.scully@gmail.com>
2013-05-26 16:15:29 -0500

    Initial stab at auth using flask-security.  Nothing currently protected but scaffold in place.

--- datamart/models.py
+++ datamart/models.py
@@ -1,6 +1,45 @@
 from datamart import db
 from sqlalchemy.ext.mutable import MutableDict
-from sqlalchemy.dialects.postgresql import HSTORE
+from sqlalchemy.dialects.postgresql import HSTORE, INET
+from flask.ext.security import RoleMixin, UserMixin
+
+roles_users = db.Table('roles_users',
+                       db.Column('user_id', db.Integer(),
+                                 db.ForeignKey('user.id')),
+                       db.Column('role_id', db.Integer(),
+                                 db.ForeignKey('role.id')))
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    last_login_at = db.Column(db.DateTime())
+    current_login_at = db.Column(db.DateTime())
+    last_login_ip = db.Column(INET)
+    current_login_ip = db.Column(INET)
+    login_count = db.Column(db.Integer())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
+
+    def approved_variables(self):
+        valid_vars = []
+
+        for role in self.roles:
+            for variable in role.variables:
+                valid_vars.append(variable.id)
+
+        return valid_vars
+
+    def __repr__(self):
+        return 'User %r, %r' % (self.username, self.email)
 
 class Dimension(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -23,6 +62,8 @@ class Variable(db.Model):
     dimension_id = db.Column(db.Integer, db.ForeignKey('dimension.id'))
     # Here Dimension is upper case because it expects the class
     dimension = db.relationship('Dimension', backref=db.backref('variables', lazy='dynamic'))
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
+    role = db.relationship('Role', backref=db.backref('variables', lazy='dynamic'))
 
     def __repr__(self):
         repr = 'Variable %r - dim: %r, ' % (self.display_name, self.dimension)
--- dev_config.py
+++ dev_config.py
@@ -5,3 +5,12 @@ BOOTSTRAP_FONTAWESOME = True
 SECRET_KEY = "\xdb\xf1\xf6\x14\x88\xd4i\xda\xbc/E'4\x7f`iz\x98r\xb9s\x1c\xca\xcd"
 CSRF_ENABLED = True
 SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://datamart:datamart@localhost/datamart'
+MAIL_SERVER = 'localhost'
+MAIL_PORT = 25
+#MAIL_USE_SSL = True
+#MAIL_USERNAME = 'username'
+#MAIL_PASSWORD = 'password'
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')

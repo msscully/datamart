599a400b74fbfca6c46715a6491ae6ee3dd0be19
Mark Scully <mark.s.scully@gmail.com>
2013-06-05 11:45:16 -0500

    Now using Flask-SSLify - Resolves Issue #20.

--- datamart/__init__.py
+++ datamart/__init__.py
@@ -5,6 +5,7 @@ from flask.ext.security import Security, SQLAlchemyUserDatastore, \
 from flask_mail import Mail
 from flask_debugtoolbar import DebugToolbarExtension
 from flask.ext.uploads import configure_uploads, UploadSet
+from flask_sslify import SSLify
 
 
 app = Flask(__name__)
@@ -13,6 +14,8 @@ app.config.from_envvar('DATAMART_APP_SETTINGS')
 
 data_files = UploadSet('data',extensions=('txt','csv'))
 configure_uploads(app, (data_files))
+
+sslify = SSLify(app)
 
 mail = Mail(app)
 

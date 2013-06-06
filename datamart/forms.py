b4331392a195a185b9da1bf938a290065fb510e4
Mark Scully <mark.s.scully@gmail.com>
2013-06-06 15:07:27 -0500

    Removed the ExampleForm.

--- datamart/forms.py
+++ datamart/forms.py
@@ -4,15 +4,6 @@ from datamart import models, db
 from wtforms.ext.sqlalchemy.orm import model_form
 from secure_redirect import RedirectForm
 
-class ExampleForm(RedirectForm):
-    field1 = TextField('First Field', description='This is field one.')
-    field2 = TextField('Second Field', description='This is field two.',
-                       validators=[Required()])
-    hidden_field = HiddenField('You cannot see this', description='Nope')
-
-    def validate_hidden_field(form, field):
-        raise ValidationError('Always wrong')
-
 class FileUploadForm(RedirectForm):
     header_row = BooleanField('Is the first column header names?')
     data_file = FileField()

a5e4c0fc01e8eade07be7f1872d538461b12018c
Mark Scully <mark.s.scully@gmail.com>
2013-07-31 12:41:11 -0500

    Ref #7: js interface for nested filters. Still rough.

--- datamart/static/js/facts.js
+++ datamart/static/js/facts.js
@@ -21,6 +21,9 @@
           return new_columns;
       };
 
+      var filterTemplate = Handlebars.getTemplate('facts-new-filter');
+      Handlebars.registerPartial("filter", filterTemplate);
+
       var columns = getColumns();
       var operations = [
           {name: '=='},
@@ -85,11 +88,68 @@
       });
 
       $(document).on('click', '#dg-add-filter', function(){
-          var compiledTemplate = Handlebars.getTemplate('facts-new-filter');
+          var compiledTemplate = Handlebars.getTemplate('facts-nested-filter');
           var columns_no_controls = columns.slice(0,-1);
-          var newFilter = compiledTemplate({columns: columns_no_controls, ops: operations});
+          var newFilter = compiledTemplate({columns: columns_no_controls, ops: operations, level: "filter0"});
+          if ($('#dg-add-filter').attr('value') === 'Show filters'){
+              $('#dg-add-filter').attr('value','Hide filters');
+              if (!$('.nested-filter-and-level').length) {
+                  $('#filter-submit').siblings('br').before(newFilter);
+              }
+              $('#fact-filter-form').slideDown()
+          }
+          else {
+              $('#dg-add-filter').attr('value','Show filters');
+              $('#fact-filter-form').slideToggle()
+          }
 
-          $('#facts-grid-filter').append($("<div class='fact-filter'></div>").html(newFilter));
+      });
+
+      var getNewLevel = function(classStr){
+          var newLevel = '';
+          if (classStr.indexOf('filter0') !== -1){
+              newLevel = "filter1";
+          } else {
+              newLevel = "filter0";
+          }
+          return newLevel;
+      }
+
+      $(document).on('change', '.and-or-select', function(){
+          currentVal = $(this).val();
+          if ($(this).parent('div.and-or').siblings('.nested-filters').children('fieldset').length <= 1) {
+              var compiledTemplate = Handlebars.getTemplate('facts-nested-filter');
+              var columns_no_controls = columns.slice(0,-1);
+              currentClass = $(this).parent('div.and-or').siblings('.nested-filters').attr('class');
+              var newLevel = getNewLevel(currentClass);
+
+              var newFilter = compiledTemplate({columns: columns_no_controls, ops: operations, level: newLevel});
+              var newClass = 'nested-filters ' + newLevel;
+              $(this).parent('div.and-or').siblings('.nested-filters').attr('class', newClass);
+              $fieldset = $(this).parent('div.and-or').parent('fieldset');
+              $fieldset.wrap("<fieldset class='nested-filter-and-level'><div class='" + currentClass + "'></div></fieldset>");
+              $fieldset.parent('.nested-filters').before("<div class='and-or'><select class='and-or-select'><option value='and'>AND</option><option value='or'>OR</option></select></div>");
+              $fieldset.parent('.nested-filters').siblings('div.and-or').children('select').val(currentVal);
+              $fieldset.after(newFilter);
+              $fieldset.siblings('.nested-filter-and-level:last').after("<button class='btn add-filter-btn'><i class='icon-plus'></i></button>");
+              $(this).val('');
+          }
+      });
+
+      $(document).on('click', 'button.btn.add-filter-btn', function(e){
+          e.preventDefault();
+          var compiledTemplate = Handlebars.getTemplate('facts-nested-filter');
+          var columns_no_controls = columns.slice(0,-1);
+          var currentClass = $(this).siblings('fieldset:first').children('div.nested-filters').attr('class');
+          if (currentClass.indexOf('filter0') !== -1){
+              currentLevel = "filter0";
+          } else {
+              currentLevel = "filter1";
+          }
+
+          var newFilter = compiledTemplate({columns: columns_no_controls, ops: operations, level: currentLevel});
+          $(this).before(newFilter)
+          return false;
       });
 
       $(document).on('change', 'select.op-select', function(){
@@ -101,30 +161,70 @@
           }
       });
 
-      $(document).on('change', '#facts-grid-filter', function(){
-          filters = [];
-          $(this).find('.fact-filter').each(function(){
-              if (($(this).find('select.field-select option:selected').val() != '') &&
-                      ($(this).find('select.op-select option:selected').val() != '') &&
-                          ($(this).find('.val-input').val() != '')){
-                  newFilter = {name: $(this).find('select.field-select option:selected').val(),
-                      op: $(this).find('select.op-select option:selected').val(),
-                      val: $(this).find('.val-input').val()
-                  };
-                  filters.push(newFilter);
-              }
-          });
+      var buildFilters = function(currentFilter){
+          filters = {};
+          value = $(currentFilter).children('div.and-or').children('select').val();
+          if (value === 'and'){
+              buildFilters($(currentFilter).children('.nested-filters'));
+              var andLevel = $(currentFilter).children('.nested-filters').children('fieldset.nested-filter-and-level');
+              nestedFilters = $.map(andLevel, function(val, i) {return buildFilters(val);});
+              return {'and': nestedFilters};
 
+          } else if (value === 'or'){
+              return {'or':[buildFilters($(currentFilter).children('.nested-filters'))]}
+
+          } else {
+              filter = $(currentFilter).children('.nested-filters').children('fieldset.nested-filter');
+              if (($(filter).find('select.field-select option:selected').val() != '') &&
+                  ($(filter).find('select.op-select option:selected').val() != '') &&
+                      ($(filter).find('.val-input').val() != '')){
+                  newFilter = {name: $(filter).find('select.field-select option:selected').val(),
+                      op: $(filter).find('select.op-select option:selected').val(),
+                      val: $(filter).find('.val-input').val()
+                  };
+                  return newFilter;
+              }
+          }
+      };
+
+
+      var clearEmptyFilters = function(filter) {
+          if ($(filter).children('div.nested-filters').children('fieldset.nested-filter-and-level').length){
+              $.each($(filter).children('div.nested-filters').children('fieldset.nested-filter-and-level'), function(index, value){
+                  clearEmptyFilters(value);
+              });
+          }
+
+          if ((! $(filter).children('div.nested-filters').children('fieldset.nested-filter').length) && (! $(filter).children('div.nested-filters').children('fieldset.nested-filter-and-level').length)){
+              $(filter).remove();
+          }
+      };
+
+      $(document).on('click','.remove-filter',function(){
+          $(this).closest('fieldset.nested-filter-and-level').remove();
+          clearEmptyFilters($('form > fieldset'));
+      });
+
+      $(document).on('click','#filter-submit', function(e){
+          e.preventDefault();
+          filters = buildFilters($('form > fieldset'));
           if (!_.isEqual($('#FactsGrid').datagrid().data().datagrid.options.dataOptions.filter,filters)) {
               $('#FactsGrid').datagrid().data().datagrid.options.dataOptions.filter = filters;
               $('#FactsGrid').datagrid('reload');
           }
-
+          return false;
       });
 
-      $(document).on('click','.remove-filter',function(){
-          $(this).closest('div.fact-filter').remove();
-          $('#facts-grid-filter').change();
+      $(document).on('click','#filter-reset', function(e){
+          e.preventDefault();
+          $('form > fieldset').remove();
+          var compiledTemplate = Handlebars.getTemplate('facts-nested-filter');
+          var columns_no_controls = columns.slice(0,-1);
+          var newFilter = compiledTemplate({columns: columns_no_controls, ops: operations, level: "filter0"});
+          $('#filter-submit').siblings('br').before(newFilter);
+          $('#FactsGrid').datagrid().data().datagrid.options.dataOptions.filter = {};
+          $('#FactsGrid').datagrid('reload');
+          return false;
       });
 
       $(document).on('click','#dg-fv-DownloadFacts',function(){
